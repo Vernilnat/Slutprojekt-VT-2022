8bac1e7e157fe62a716b40c413792640bca91093
Vernilnat <vernilnat@gmail.com>
2022-03-27 00:27:20 +0100

    .

--- main.py
+++ main.py
@@ -1,15 +1,71 @@
+#
+# <formel>::= <mol> \n
+# <mol>   ::= <group> | <group><mol>
+# <group> ::= <atom> |<atom><num> | (<mol>) <num>
+# <atom>  ::= <LETTER> | <LETTER><letter>
+# <LETTER>::= A | B | C | ... | Z
+# <letter>::= a | b | c | ... | z
+# <num>   ::= 2 | 3 | 4 | ...
+#
+# Pseudokod:
+#
+# Dela upp inmatade formler
+# Titta på första gruppen i formeln
 
-def readformula():
-    readmol()
-def readmol():
-    pass
+
+# Grundämnen
+atoms = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca',
+         'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y',
+         'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La',
+         'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re',
+         'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np',
+         'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg',
+         'Cn', 'Fl', 'Lv']
+
+#Jag tror jag borde fixa ett kösystem
+#
+
+# Formeln måste börja med "(" eller stor bokstav
+def readformula(formula):
+
+    readgroup(firstgroup)
+
+
 def readgroup():
     pass
+
+
 def readatom():
     pass
+
+
 def readletter():
     pass
+
+
 def readnum():
     pass
 
 
+def parse_compunds(compunds):
+    formula = compunds[0]
+    readformula(formula)
+    return compunds
+
+
+def main():
+    compunds = []
+    print("Mata in kemiska föreningar här:")
+    while True:
+        substance = input("")
+        if substance == "#":
+            break
+        compunds.append(substance)
+    results = parse_compunds(compunds)
+    print(results)
+
+
+# Startar programmet om programmet startar i denna fil. (Startar ej programmet ifall det kallas från en annan fil)
+# (Behövs egentligen inte här)
+if __name__ == "__main__":
+    main()
